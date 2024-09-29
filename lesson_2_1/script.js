28c4db4b7fc1bd0d2145244b63f2f56b01ad9f50
dimetry537 <maksakov1988@gmail.com>
2024-09-29 20:21:59 +0300

    add lesson 2.1

---
+++ lesson_2_1/script.js
@@ -0,0 +1,4 @@
+console.log('Исправил скрипт 2');
+alert('Вы подключили внешний скрипт!');
+console.log('Текущее время:', new Date().toLocaleTimeString());
+alert('"Это дополнительное сообщение!" ')
